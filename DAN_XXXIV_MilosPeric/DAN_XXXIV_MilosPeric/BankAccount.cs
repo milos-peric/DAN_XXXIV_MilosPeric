c192427c4cb82789c12f0ed43458039cbb8b2022
milos.peric <steamestt@gmail.com>
2020-06-18 09:18:28 +0200

    Add Project files

---
+++ DAN_XXXIV_MilosPeric/DAN_XXXIV_MilosPeric/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_XXXIV_MilosPeric
+{
+    class BankAccount
+    {
+        public decimal Amount { get; }
+
+        public BankAccount(decimal amount)
+        {
+            Amount = amount;
+        }
+
+        public void PrintAccountBalance()
+        {
+            Console.WriteLine("Acount balance: " + Amount);
+        }
+    }
+}
---
+++ DAN_XXXIV_MilosPeric/DAN_XXXIV_MilosPeric/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_XXXIV_MilosPeric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount racun = new BankAccount(1000);
+            Console.ReadKey();
+        }
+    }
+}
